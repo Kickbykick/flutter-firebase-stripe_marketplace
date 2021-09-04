6aa34545d6769275f1848acead5ae52c8d6afc92
Kickbykick <kiibatitoo@gmail.com>
2021-09-04 01:06:41 -0500

    Update cart_page.dart

--- stripe_marketplace/lib/stripe/cart_page.dart
+++ stripe_marketplace/lib/stripe/cart_page.dart
@@ -1,13 +1,15 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/credit_card_model.dart';
 import 'package:provider/provider.dart';
 import 'package:stripe_marketplace/stripe/constants.dart';
+import 'package:stripe_marketplace/stripe/credit_card_page.dart';
 import 'package:stripe_marketplace/stripe/database.dart';
 import 'package:stripe_marketplace/stripe/product.dart';
 import 'package:stripe_marketplace/stripe/sellers_dashboard.dart';
 import 'package:stripe_marketplace/stripe/user.dart';
-import 'package:stripe_payment/stripe_payment.dart';
 import 'package:http/http.dart' as http;
+import 'package:stripe_payment/stripe_payment.dart';
 
 //TODO: Switch plugin
 class CartPage extends StatefulWidget {
@@ -22,7 +24,7 @@ class CartPage extends StatefulWidget {
 
 class _CartPageState extends State<CartPage> {
   String text = 'Click the button to start the payment';
-  double totalCost = 10.0;
+  double totalCost = 1000.0;
   double tip = 1.0;
   double tax = 0.0;
   double extraAmount = 0.0;
@@ -43,36 +45,33 @@ class _CartPageState extends State<CartPage> {
     );
   }
 
-  void checkIfNativePayReady() async {
-    createPaymentMethod();
-  }
-
-  Future<void> createPaymentMethod() async {
+  Future<void> createPaymentMethod(CreditCardModel creditCardModel) async {
     StripePayment.setStripeAccount(null);
+
     tax = ((totalCost * taxPercent) * 100).ceil() / 100;
     amount = ((totalCost + tip + tax) * 100).toInt();
-    print('amount in pence/cent which will be charged = $amount');
-    //step 1: add card
-    PaymentMethod paymentMethod = PaymentMethod();
-    paymentMethod = await StripePayment.paymentRequestWithCardForm(
-      CardFormPaymentRequest(),
-    ).then((PaymentMethod paymentMethod) {
-      return paymentMethod;
-    }).catchError((e) {
-      print('Errore Card: ${e.toString()}');
+    List<String> listOfExpiryDate =
+        creditCardModel.expiryDate.split("/"); //e.g [11, 22]
+
+    print(listOfExpiryDate);
+    processPaymentAsDirectCharge({
+      "paymentmethod": {
+        "type": "card",
+        "card": {
+          "number": creditCardModel.cardNumber,
+          "exp_month": int.parse(listOfExpiryDate[0]),
+          "exp_year": int.parse('20${listOfExpiryDate[1]}'),
+          "cvc": creditCardModel.cvvCode
+        },
+        "billing_details": {
+          "name": 'Jenny Rosen',
+          "address": {"postal_code": "R2J1P6"}
+        }
+      }
     });
-    paymentMethod != null
-        ? processPaymentAsDirectCharge(paymentMethod)
-        : showDialog(
-            context: context,
-            builder: (BuildContext context) => ShowDialogToDismiss(
-                title: 'Error',
-                content:
-                    'It is not possible to pay with this card. Please try again with a different card',
-                buttonText: 'CLOSE'));
   }
 
-  Future<void> processPaymentAsDirectCharge(PaymentMethod paymentMethod) async {
+  Future<void> processPaymentAsDirectCharge(Map paymentMethod) async {
     setState(() {
       showSpinner = true;
     });
@@ -80,119 +79,86 @@ class _CartPageState extends State<CartPage> {
     // String uidPayment = "4oHYQE3z3b7WeEgjh8u7";
     String stripeAccountId = widget.accountID ?? "acct_1IhUiS2RGp15zJDq";
     String productReference = "/Product/${widget.product.id}" ?? "";
-    Map paymentMethodMap = {
-      "paymentmethod": {
-        "type": "card",
-        "card": {
-          "number": paymentMethod.card.number,
-          "exp_month": paymentMethod.card.expMonth,
-          "exp_year": paymentMethod.card.expYear,
-          "cvc": paymentMethod.card.cvc
+    print(stripeAccountId);
+
+    try {
+      print(paymentMethod);
+      final http.Response response = await http.post(
+        Uri.parse(
+            '$stripepaymentCloudUrl?accountid=$stripeAccountId&reference=$productReference&paymentMethod=$paymentMethod'),
+        body: json.encode(paymentMethod),
+        headers: {
+          'Content-type': 'application/json',
         },
-        "billing_details": {
-          "name": paymentMethod.billingDetails.name,
-          "address": {"postal_code": paymentMethod.billingDetails.address}
-        }
-      }
-    };
+      );
 
-    final http.Response response = await http.post(
-      Uri.parse(
-          '$stripepaymentCloudUrl?accountid=$stripeAccountId&reference=$productReference&paymentmethod=${paymentMethod.toJson()}'),
-      body: json.encode(paymentMethodMap),
-      headers: {
-        'Content-type': 'application/json',
-      },
-    );
+      print(response.body);
 
-    print('Now i decode \n ${response.body} --- \n${paymentMethod.toJson()}');
+      if (response.body != null && response.body != 'error') {
+        final paymentIntentX = jsonDecode(response.body);
+        final status = paymentIntentX['paymentIntent']['status'];
+        final strAccount = paymentIntentX['stripeAccount'];
+        //step 3: check if payment was succesfully confirmed
+        if (status == 'succeeded') {
+          StripePayment.completeNativePayRequest();
 
-    if (response.body != null && response.body != 'error') {
-      final paymentIntentX = jsonDecode(response.body);
-      final status = paymentIntentX['paymentIntent']['status'];
-      final strAccount = paymentIntentX['stripeAccount'];
-      //step 3: check if payment was succesfully confirmed
-      if (status == 'succeeded') {
-        StripePayment.completeNativePayRequest();
-        setState(() {
-          text =
-              'Payment completed. ${paymentIntentX['paymentIntent']['amount'].toString()}p succesfully charged';
-          showSpinner = false;
-        });
-      } else {
-        StripePayment.setStripeAccount(strAccount);
-        await StripePayment.confirmPaymentIntent(PaymentIntent(
-                paymentMethodId: paymentIntentX['paymentIntent']
-                    ['payment_method'],
-                clientSecret: paymentIntentX['paymentIntent']['client_secret']))
-            .then(
-          (PaymentIntentResult paymentIntentResult) async {
-            final statusFinal = paymentIntentResult.status;
-            if (statusFinal == 'succeeded') {
-              StripePayment.completeNativePayRequest();
-              await DatabaseService(
-                      uid: Provider.of<User>(context, listen: false).uid)
-                  .createHistoryDocumentForUser(productReference, totalCost,
-                      tax, extraAmount, tip, widget.product.sellerid);
-              setState(() {
-                showSpinner = false;
-              });
-              // Generate history for user - uid, product id, brefund false
-            } else if (statusFinal == 'processing') {
-              StripePayment.cancelNativePayRequest();
-              setState(() {
-                showSpinner = false;
-              });
-              showDialog(
-                  context: context,
-                  builder: (BuildContext context) => ShowDialogToDismiss(
-                      title: 'Warning',
-                      content:
-                          'The payment is still in \'processing\' state. This is unusual. Please contact us',
-                      buttonText: 'CLOSE'));
-            } else {
-              StripePayment.cancelNativePayRequest();
-              setState(() {
-                showSpinner = false;
-              });
-              showDialog(
-                  context: context,
-                  builder: (BuildContext context) => ShowDialogToDismiss(
-                      title: 'Error',
-                      content:
-                          'There was an error to confirm the payment. Details: $statusFinal',
-                      buttonText: 'CLOSE'));
-            }
-          },
-          //If Authentication fails, a PlatformException will be raised which can be handled here
-        ).catchError((e) {
-          //case B1
-          StripePayment.cancelNativePayRequest();
-          setState(() {
-            showSpinner = false;
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text(
+                  'Payment completed. ${paymentIntentX['paymentIntent']['amount'].toString()}\$ succesfully charged')));
+        } else {
+          StripePayment.setStripeAccount(strAccount);
+          await StripePayment.confirmPaymentIntent(PaymentIntent(
+                  paymentMethodId: paymentIntentX['paymentIntent']
+                      ['payment_method'],
+                  clientSecret: paymentIntentX['paymentIntent']
+                      ['client_secret']))
+              .then(
+            (PaymentIntentResult paymentIntentResult) async {
+              final statusFinal = paymentIntentResult.status;
+              if (statusFinal == 'succeeded') {
+                StripePayment.completeNativePayRequest();
+                await DatabaseService(
+                        uid: Provider.of<User>(context, listen: false).uid)
+                    .createHistoryDocumentForUser(productReference, totalCost,
+                        tax, extraAmount, tip, widget.product.sellerid);
+                setState(() {
+                  showSpinner = false;
+                });
+                // Generate history for user - uid, product id, brefund false
+              } else if (statusFinal == 'processing') {
+                StripePayment.cancelNativePayRequest();
+
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text(
+                        'The payment is still in \'processing\' state. This is unusual. Please contact us')));
+              } else {
+                StripePayment.cancelNativePayRequest();
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text(
+                        'There was an error to confirm the payment. Details: $statusFinal')));
+              }
+            },
+            //If Authentication fails, a PlatformException will be raised which can be handled here
+          ).catchError((e) {
+            //case B1
+            StripePayment.cancelNativePayRequest();
+
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: Text(
+                    'There was an error to confirm the payment. Please try again with another card')));
           });
-          showDialog(
-              context: context,
-              builder: (BuildContext context) => ShowDialogToDismiss(
-                  title: 'Error',
-                  content:
-                      'There was an error to confirm the payment. Please try again with another card',
-                  buttonText: 'CLOSE'));
-        });
+        }
+      } else {
+        //case A
+        StripePayment.cancelNativePayRequest();
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text(
+                'There was an error in creating the payment. Please try again with another card')));
       }
-    } else {
-      //case A
-      StripePayment.cancelNativePayRequest();
-      setState(() {
-        showSpinner = false;
-      });
-      showDialog(
-          context: context,
-          builder: (BuildContext context) => ShowDialogToDismiss(
-              title: 'Error',
-              content:
-                  'There was an error in creating the payment. Please try again with another card',
-              buttonText: 'CLOSE'));
+    } catch (e) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Error: $e')));
+      rethrow;
     }
   }
 
@@ -227,7 +193,16 @@ class _CartPageState extends State<CartPage> {
                                 style: TextStyle(fontSize: 40),
                               ),
                               onPressed: () async {
-                                await createPaymentMethod();
+                                await Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => CreditCardPage(),
+                                  ),
+                                ).then((creditCartModel) async {
+                                  if (creditCartModel is CreditCardModel) {
+                                    await createPaymentMethod(creditCartModel);
+                                  }
+                                });
                               },
                             ),
                           ),
